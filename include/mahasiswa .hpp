901d16a435a2c0acf0484d4a2e50b6d2a91f6e29
Marcel Aritonang <62584017+marcelaritonang@users.noreply.github.com>
2022-06-11 22:53:23 +0700

    Update mahasiswa .hpp

--- include/mahasiswa .hpp
+++ include/mahasiswa .hpp
@@ -1,51 +1,24 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@marcelaritonang 
-rezafuad
-/
-prolan-sim
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-prolan-sim/include/mahasiswa.hpp
-@rezafuad
-rezafuad first commit
-Latest commit 09aa7ec 13 days ago
- History
- 1 contributor
-35 lines (26 sloc)  661 Bytes
-   
 #ifndef __MAHASISWA_HPP__
 #define __MAHASISWA_HPP__
 
 
 #include <iostream>
 #include "include/person.hpp"
+ using namespace std;
 
 class mahasiswa : public person {
 private:
-	std::string nrp;
-	std::string departemen;
+	string nrp;
+	string DPT;
 	int tahunmasuk;
 	int semesterke;
 	int skslulus;
 	float ipk;
-	std::vector<float> ips;
+	vector<float> ips;
+	vector<string> matkul;
 
 public:
-	mahasiswa(std::string id, std::string nama, int dd, int mm, int yy,
-					std::string nrp, std::string departemen, int tahunmasuk);
+	mahasiswa(string id, string nama, int dd, int mm, int yy, string nrp, string DPT, int tahunmasuk, vector<string> matkul);
 
 	void setSemester(int semesterke);
 	int getSemester();
@@ -57,20 +30,16 @@ public:
 
 	void setIPS(int semester, float ips);
 	float getIPS(int semester);
-	std::vector<float> getAllIPS();
+	 vector<float> getAllIPS();
+	string getNRP();
+
+	void setDPT(string DPT);
+	string getDPT();
+
+	int getTahunMasuk();
+
+	void setMatkul(string matkul);
+	vector<string> getMatkul();
 };
 
 #endif
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading complete
